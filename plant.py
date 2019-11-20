607cfba4b07fe12e82231e4b5f5c73d8a43cdeb6
Estella Song <estellajaysong@Estellas-MBP.hitronhub.home>
2019-11-19 22:09:52 -0800

    first commit

---
+++ plant.py
@@ -0,0 +1,6 @@
+class Plant:
+  def __init__(self, type, name, last_watered = None, last_fed = None):
+    self.type = type
+    self.name = name
+    self.last_watered = last_watered
+    self.last_fed = last_fed
